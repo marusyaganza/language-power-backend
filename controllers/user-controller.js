8bd3c146bf75431f1cde3c90460240a0558542b5
Marusya Ganza <maursyaganza@yandex.ru>
2020-04-25 13:22:25 +0300

    hotfix: disable signup on prod

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -64,6 +64,9 @@ async function login(req, res, next) {
 
 async function signup(req, res, next) {
     const {name, email, password} = req.body;
+    if (!process.env.IS_SIGNUP_AVAILABLE) {
+        return next(HttpError('Sign Up is temporarily unavailable, please try again later', 503));
+    }
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         console.error(errors);
