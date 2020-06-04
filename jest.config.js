a0a374e2ab922e9ee8337bd6f6647101a1ebfcbd
Marusya Ganza <maursyaganza@yandex.ru>
2020-06-04 09:13:14 +0300

    add test threshold and travis

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,23 @@ module.exports = {
   coveragePathIgnorePatterns: ['__tests__', 'node_modules'],
   testMatch: ['**/__tests__/**/*.test.js'],
   testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      lines: 80,
+      functions: 80,
+    },
+    'controllers/**/*.js': {
+      branches: 50,
+      lines: 80,
+      functions: 80,
+      statements: -10,
+    },
+    'middleware/**/*.js': {
+      branches: 50,
+      lines: 80,
+      functions: 80,
+      statements: -10,
+    },
+  },
 };
