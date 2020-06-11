2e70a636dd13d9e6ed25aaaa042e5243302f7032
Marusya Ganza <maursyaganza@yandex.ru>
2020-06-11 23:12:24 +0300

    fix: fix query uppercase handling

--- controllers/__tests__/format-data.js
+++ controllers/__tests__/format-data.js
@@ -4,136 +4,159 @@ const noMeta = require('./mocks/no-meta-data.json');
 const noAudio = require('./mocks/no-audio.json');
 const noDeffs = require('./mocks/no-deff.json');
 
-
 const data = [
-    {
-        input: {
-            name: 'empty',
-            array: [],
-            query: ''
-        },
-        output: {
-            suggestions: [],
-            match: [],
-            related: []
-        }
+  {
+    input: {
+      name: 'empty',
+      array: [],
+      query: '',
     },
-    {
-        input: {
-            name: 'homonym',
-            array: shoal,
-            query: 'shoal'
-        },
-        output: {
-            "suggestions": [],
-            "match": [
-                {
-                    "uuid": "74046e79-e4c9-4b52-ac96-cb7ae98fb601",
-                    "name": "shoal",
-                    "stems": [
-                        "shoal",
-                        "shoals"
-                    ],
-                    "defs": [
-                        "{bc} a large group or number",
-                        "{bc} an area where the water in a sea, lake, or river is not deep",
-                        "{bc} a small, raised area of sand just below the surface of the water"
-                    ],
-                    "particle": "noun",
-                    "examples": [
-                        "fish lurking in the {it}shoals{/it}",
-                        "The boat ran aground on the {it}shoal{/it}.",
-                        "a {it}shoal{/it} [=(more commonly) {it}school{/it}] {it}of{/it} fish",
-                        "({it}chiefly Brit{/it}) a {it}shoal of{/it} letters from angry readers"
-                    ],
-                    "pronunciation": {
-                        "transcription": "ˈʃoʊl",
-                        "audioUrl": "https://test.com/audio/s/shoal001.wav"
-                    }
-                }
-            ],
-            "related": []
-        }
+    output: {
+      suggestions: [],
+      match: [],
+      related: [],
     },
-    {
-        input: {
-            name: 'suggestions',
-            array: ["court", "cor"],
-            query: 'cot'
-        },
-        output: {
-            suggestions: ["court", "cor"],
-            match: [],
-            related: []
-        }  
+  },
+  {
+    input: {
+      name: 'homonym',
+      array: shoal,
+      query: 'shoal',
     },
-    {
-        input: {
-            name: 'empty defs',
-            array: doll,
-            query: 'doll'
-        },
-        output: {
-            suggestions: [],
-            match: [],
-            related: []
-        }  
-    },
-    {
-        input: {
-            name: 'empty metadata',
-            array: noMeta,
-            query: 'doll'
-        },
-        output: {
-            suggestions: [],
-            match: [],
-            related: []
-        }  
-    },
-    {
-        input: {
-            name: 'empty audio',
-            array: noAudio,
-            query: 'demure'
-        },
-        output: {
-            "suggestions": [],
-    "match": [
+    output: {
+      suggestions: [],
+      match: [
         {
-            "uuid": "43afd541-00c2-442f-a855-485c2d3be1af",
-            "name": "demure",
-            "stems": [
-                "demure",
-                "demurely",
-                "demureness"
-            ],
-            "defs": [
-                "{bc} quiet and polite",
-                "{bc} not attracting or demanding a lot of attention {bc} not showy or flashy"
-            ],
-            "particle": "adjective",
-            "examples": [],
-            "pronunciation": {}
-        }
-    ],
-    "related": []
-        }  
-    },
-    {
-        input: {
-            name: 'no defs',
-            array: noDeffs,
-            query: 'demure'
+          uuid: '74046e79-e4c9-4b52-ac96-cb7ae98fb601',
+          name: 'shoal',
+          stems: ['shoal', 'shoals'],
+          defs: [
+            '{bc} a large group or number',
+            '{bc} an area where the water in a sea, lake, or river is not deep',
+            '{bc} a small, raised area of sand just below the surface of the water',
+          ],
+          particle: 'noun',
+          examples: [
+            'fish lurking in the {it}shoals{/it}',
+            'The boat ran aground on the {it}shoal{/it}.',
+            'a {it}shoal{/it} [=(more commonly) {it}school{/it}] {it}of{/it} fish',
+            '({it}chiefly Brit{/it}) a {it}shoal of{/it} letters from angry readers',
+          ],
+          pronunciation: {
+            transcription: 'ˈʃoʊl',
+            audioUrl: 'https://test.com/audio/s/shoal001.wav',
+          },
         },
-        output: {
-            suggestions: [],
-            match: [],
-            related: []
-        }  
+      ],
+      related: [],
     },
-]
+  },
+  {
+    input: {
+      name: 'upper case query',
+      array: shoal,
+      query: 'Shoal',
+    },
+    output: {
+      suggestions: [],
+      match: [
+        {
+          uuid: '74046e79-e4c9-4b52-ac96-cb7ae98fb601',
+          name: 'shoal',
+          stems: ['shoal', 'shoals'],
+          defs: [
+            '{bc} a large group or number',
+            '{bc} an area where the water in a sea, lake, or river is not deep',
+            '{bc} a small, raised area of sand just below the surface of the water',
+          ],
+          particle: 'noun',
+          examples: [
+            'fish lurking in the {it}shoals{/it}',
+            'The boat ran aground on the {it}shoal{/it}.',
+            'a {it}shoal{/it} [=(more commonly) {it}school{/it}] {it}of{/it} fish',
+            '({it}chiefly Brit{/it}) a {it}shoal of{/it} letters from angry readers',
+          ],
+          pronunciation: {
+            transcription: 'ˈʃoʊl',
+            audioUrl: 'https://test.com/audio/s/shoal001.wav',
+          },
+        },
+      ],
+      related: [],
+    },
+  },
+  {
+    input: {
+      name: 'suggestions',
+      array: ['court', 'cor'],
+      query: 'cot',
+    },
+    output: {
+      suggestions: ['court', 'cor'],
+      match: [],
+      related: [],
+    },
+  },
+  {
+    input: {
+      name: 'empty defs',
+      array: doll,
+      query: 'doll',
+    },
+    output: {
+      suggestions: [],
+      match: [],
+      related: [],
+    },
+  },
+  {
+    input: {
+      name: 'empty metadata',
+      array: noMeta,
+      query: 'doll',
+    },
+    output: {
+      suggestions: [],
+      match: [],
+      related: [],
+    },
+  },
+  {
+    input: {
+      name: 'empty audio',
+      array: noAudio,
+      query: 'demure',
+    },
+    output: {
+      suggestions: [],
+      match: [
+        {
+          uuid: '43afd541-00c2-442f-a855-485c2d3be1af',
+          name: 'demure',
+          stems: ['demure', 'demurely', 'demureness'],
+          defs: [
+            '{bc} quiet and polite',
+            '{bc} not attracting or demanding a lot of attention {bc} not showy or flashy',
+          ],
+          particle: 'adjective',
+          examples: [],
+          pronunciation: {},
+        },
+      ],
+      related: [],
+    },
+  },
+  {
+    input: {
+      name: 'no defs',
+      array: noDeffs,
+      query: 'demure',
+    },
+    output: {
+      suggestions: [],
+      match: [],
+      related: [],
+    },
+  },
+];
 module.exports = data;
-
-
-  
